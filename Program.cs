3d96b00fa180b55aba6cb1467eadbbbb37a2afed
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-11-22 22:26:59 -0300

    Versão inicial

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        int i = 1; 
+ 
+        while (i <= 40)
+        {
+            if (i % 4 == 0)
+            {
+                Console.Write("pim");
+            }
+            else
+            {
+                Console.Write(i);
+            }
+ 
+            if (i % 4 == 0) 
+            {
+                Console.WriteLine();
+            }
+            else
+            {
+                Console.Write(" ");
+            }
+ 
+            i++; 
+        }
+    }
+}
